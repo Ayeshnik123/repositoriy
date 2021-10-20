30fc2cadfd086e9813308c0c87de459b9dd1a947
Сергей Кучаев <serejakuchaev.com38@gmail.com>
2021-10-20 08:16:12 +0300

    1

---
+++ Репозиторий/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Репозиторий
+{
+    class Class1
+    {
+        public static void Zapolnit(DataGridView dataGridView1, DataGridView dataGridView2)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < dataGridView1.ColumnCount; i++)
+                for (int j = 0; j < dataGridView1.RowCount; j++)
+                {
+                    dataGridView1[i, j].Value = rand.Next(-10,10);
+                }
+            for (int i = 0; i < dataGridView2.ColumnCount; i++)
+                for (int j = 0; j < dataGridView2.RowCount; j++)
+                {
+                    dataGridView2[i, j].Value = rand.Next(-10, 10);
+                }
+        }
+
+        public static void clear (DataGridView dataGridView1, DataGridView dataGridView2)
+        {
+            dataGridView1.Columns.Clear();
+            dataGridView1.Rows.Clear();
+            dataGridView2.Columns.Clear();
+            dataGridView2.Rows.Clear();
+            dataGridView1.ColumnCount = 1;
+            dataGridView1.RowCount = 1;
+            dataGridView2.ColumnCount = 1;
+            dataGridView2.RowCount = 1;
+
+        }
+
+        public static void zadanie(DataGridView dataGridView1, DataGridView dataGridView2, TextBox textBox1, TextBox textBox2)
+        {
+            int max = 0;
+            for (int j = 0; j <dataGridView1.RowCount; j ++)
+                for (int i = 0; i < dataGridView1.ColumnCount; i++)
+                {
+                    if (Convert.ToInt32(dataGridView1[0, i].Value) < 0) max *= Convert.ToInt32(dataGridView1[0, i].Value);
+                }
+                textBox1.Text = Convert.ToString(max);
+           
+        }
+    }
+}
--- Репозиторий/Form1.cs
+++ Репозиторий/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Репозиторий
 {
     public partial class Form1 : Form
@@ -20,6 +21,69 @@ namespace Репозиторий
         private void button1_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            dataGridView1.RowCount = Convert.ToInt32(numericUpDown1.Value);
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            dataGridView1.ColumnCount = Convert.ToInt32(numericUpDown2.Value);
+        }
+
+        private void numericUpDown4_ValueChanged(object sender, EventArgs e)
+        {
+            dataGridView2.RowCount = Convert.ToInt32(numericUpDown4.Value);
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            dataGridView2.ColumnCount = Convert.ToInt32(numericUpDown3.Value);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Class1.Zapolnit(dataGridView1, dataGridView2);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Class1.clear(dataGridView1, dataGridView2);
+            numericUpDown1.Value = 1;
+            numericUpDown2.Value = 1;
+            numericUpDown3.Value = 1;
+            numericUpDown4.Value = 1;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Даны два двумерных массива вещественных элементов. Размер исходных массивов не превосходит" +
+                "10х10 элементов.Для каждого из массивов указать номера столбцов, произведение отрицательных" +
+                "элементов которых является положительным числом.Если таких столбцов нет ни для одного из" +
+                "массивов, то вывести соответствующее сообщение.Проверку столбца на выполнение условия и расчет" +
+                "оформить в виде процедуры с передачей в нее всех элементов текущего столбца.");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Извиняюсь тысячикратно");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Прощаю");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Class1.zadanie(dataGridView1, dataGridView2, textBox1, textBox2);
         }
     }
 }
